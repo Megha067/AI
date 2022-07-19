cefccc7ee316ddf43f4dcaec42361b832e9878a3
Megha_067 <meghagoriya067@gmail.com>
2022-07-19 22:13:52 +0530

    Initial commit

---
+++ Flask/utils.py
@@ -0,0 +1,92 @@
+disease_dic = {
+    'Apple___Apple_scab': """ <b>Crop</b>: Apple <br/>Disease: Apple Scab<br/>  """,
+    'Apple___Black_rot': """ <b>Crop</b>: Apple <br/>""",
+    'Apple___Cedar_apple_rust': """ <b>Crop</b>: Apple <br/>""",
+    'Apple___healthy': """ <b>Crop</b>: Apple <br/>""",
+    'Blueberry___healthy': """ <b>Crop</b>: Blueberry <br/>""",
+    'Cherry_(including_sour)___Powdery_mildew': """ <b>Crop</b>: Cherry <br/>""",
+    'Cherry_(including_sour)___healthy': """ <b>Crop</b>: Cherry <br/>""",
+
+    'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot': """ <b>Crop</b>: Corn <br/>""",
+
+    'Corn_(maize)___Common_rust_': """ <b>Crop</b>: Corn(maize) <br/> """,
+
+    'Corn_(maize)___Northern_Leaf_Blight': """ <b>Crop</b>: Corn(maize) <br/>""",
+
+    'Grape___Black_rot': """ <b>Crop</b>: Grape <br/>""",
+
+    'Corn_(maize)___healthy': """ <b>Crop</b>: Corn(maize) <br/>""",
+
+    'Grape___Esca_(Black_Measles)': """ <b>Crop</b>: Grape <br/>""",
+
+    'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)': """ <b>Crop</b>: Grape <br/>""",
+
+    'Grape___healthy': """ <b>Crop</b>: Grape <br/>""",
+
+    'Corn_(maize)___healthy': """ <b>Crop</b>: Corn(maize) <br/>""",
+
+    'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)': """<b> Crop</b> : Grape <br/> """,
+
+    'Orange___Haunglongbing_(Citrus_greening)': """ <b>Crop</b>: Orange <br/>""",
+
+    'Peach___Bacterial_spot': """ <b>Crop</b>: Peach <br/>""",
+
+
+    'Pepper,_bell___Bacterial_spot': """ <b>Crop</b>: Pepper <br/>""",
+
+    'Peach___healthy': """ <b>Crop</b>: Peach <br/>""",
+
+    'Pepper,_bell___healthy': """ <b>Crop</b>: Pepper <br/>""",
+
+
+    'Potato___healthy': """ <b>Crop</b>: Potato <br/>""",
+
+    'Raspberry___healthy': """ <b>Crop</b>: Raspberry <br/>""",
+
+
+    'Soybean___healthy': """ <b>Crop</b>: Soyabean <br/>""",
+
+    'Strawberry___healthy': """ <b>Crop</b>: Strawberry <br/>""",
+
+    'Tomato___healthy': """ <b>Crop</b>: Tomato <br/>""",
+ 
+    'Potato___Early_blight': """ <b>Crop</b>: Potato <br/>""",
+
+
+    'Potato___Late_blight': """ <b>Crop</b>: Potato <br/>""",
+
+
+    'Squash___Powdery_mildew': """ <b>Crop</b>: Squash <br/>""",
+
+
+    'Strawberry___Leaf_scorch': """ <b>Crop</b>: Strawberry <br/>""",
+
+    'Tomato___Bacterial_spot': """ <b>Crop</b>: Tomato <br/>""",
+
+
+
+    'Tomato___Early_blight': """ <b>Crop</b>: Tomato <br/>""",
+
+
+
+    'Tomato___Late_blight': """ <b>Crop</b>: Tomato <br/>""",
+
+
+
+    'Tomato___Leaf_Mold': """ <b>Crop</b>: Tomato <br/>""",
+
+
+    'Tomato___Septoria_leaf_spot': """ <b>Crop</b>: Tomato <br/>""",
+
+
+
+    'Tomato___Spider_mites Two-spotted_spider_mite': """ <b>Crop</b>: Tomato <br/>""",
+
+
+    'Tomato___Target_Spo': """ <b>Crop</b>: Tomato <br/>""",
+
+
+    'Tomato___Tomato_Yellow_Leaf_Curl_Virus': """ <b>Crop</b>: Tomato <br/>""",
+
+    'Tomato___Tomato_mosaic_virus': """ <b>Crop</b>: Tomato <br/>"""
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Plant_AI-master
+ 
